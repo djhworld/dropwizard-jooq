77da9c8f6ff8a0efc79ecd8e0ed6d93e6f1fc545
djhworld <djharperuk@gmail.com>
2014-09-28 14:06:34 +0100

    Removed imports that were no longer needed

--- src/main/java/io/github/djhworld/dropwizard/jooq/JooqDSLContextFactory.java
+++ src/main/java/io/github/djhworld/dropwizard/jooq/JooqDSLContextFactory.java
@@ -1,18 +1,16 @@
 package io.github.djhworld.dropwizard.jooq;
 
-import ch.qos.logback.classic.Logger;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.db.ManagedDataSource;
 import io.dropwizard.setup.Environment;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
-import org.slf4j.LoggerFactory;
 
 public class JooqDSLContextFactory {
     public DSLContext build(Environment environment,
-                     DataSourceFactory configuration,
-                     SQLDialect dialect) throws ClassNotFoundException {
+                            DataSourceFactory configuration,
+                            SQLDialect dialect) throws ClassNotFoundException {
         final ManagedDataSource dataSource = configuration.build(environment.metrics(), dialect.getName());
         return build(environment, configuration, dataSource, dialect);
     }
@@ -27,9 +25,9 @@ public class JooqDSLContextFactory {
      * @return
      */
     public DSLContext build(Environment environment,
-                     DataSourceFactory configuration,
-                     ManagedDataSource dataSource,
-                     SQLDialect dialect) {
+                            DataSourceFactory configuration,
+                            ManagedDataSource dataSource,
+                            SQLDialect dialect) {
         final String validationQuery = configuration.getValidationQuery();
         final DSLContext dslContext = DSL.using(dataSource, dialect);
         environment.lifecycle().manage(dataSource);
